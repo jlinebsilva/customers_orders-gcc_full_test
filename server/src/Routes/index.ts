421b7c2c5f407e3e76246efce39ab1055af29744
JLineBRSilva <j.silvabroccolo@gmail.com>
2026-04-27 20:40:02 -0300

    [server]/refactor: altera rotas '/customer' para '/customers'

--- server/src/Routes/index.ts
+++ server/src/Routes/index.ts
@@ -9,9 +9,9 @@ const routes = Router()
 routes.post("/register", sign.register);
 routes.post("/login", sign.login);
 
-routes.get("/customer", auth, customer.listCustomer);
-routes.put("/customer/:id", auth, customer.updateCustomer);
-routes.delete("/customer/:id", auth, customer.deleteCustomer);
+routes.get("/customers", auth, customer.listCustomer);
+routes.put("/customers/:id", auth, customer.updateCustomer);
+routes.delete("/customers/:id", auth, customer.deleteCustomer);
 
 routes.post("/orders", auth, order.createOrder);
 routes.get("/orders", auth, order.listOrders);
